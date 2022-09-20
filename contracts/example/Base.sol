e74c63b1ddb51f4f2e21b35b842b1129d3c0294b
greedythib <thibaud.bruyelle@gmail.com>
2022-09-20 11:29:29 +0200

    wip

--- contracts/example/Base.sol
+++ contracts/example/Base.sol
@@ -4,4 +4,23 @@ pragma solidity ^0.8.0;
 
 contract Base {
     bool public isBase = true;
+    address private _owner;
+
+    /** Errors */
+    error NotOwner();
+
+    /** Events */
+    event OwnerCall();
+
+    constructor(address owner) {
+        _owner = owner;
+    }
+
+    function isOwner() external returns (bool) {
+        if (msg.sender != _owner) revert NotOwner();
+        else {
+            emit OwnerCall();
+            return true;
+        }
+    }
 }
--- test/hardhat/Base.test.ts
+++ test/hardhat/Base.test.ts
@@ -5,16 +5,23 @@ import { ethers } from 'hardhat';
 import { Base } from '../../typechain';
 
 describe('Base tests', async function () {
-  /** NEW FEATURE - `LoadMixture` */
   async function deployContract() {
-    const [alice] = await ethers.getSigners();
+    const [alice, bob] = await ethers.getSigners();
     const base = await ethers.getContractFactory('Base');
-    const BaseContract = (await base.deploy()) as Base;
-    return { alice, BaseContract };
+    const BaseContract = (await base.deploy(alice.address)) as Base;
+    return { alice, bob, BaseContract };
   }
-
-  it('success -  check base contract', async () => {
-    const { alice, BaseContract } = await loadFixture(deployContract);
+  it('success - check `isBase()` attribute', async () => {
+    const { alice, BaseContract } = await loadFixture(deployContract); /** NEW FEATURE - `LoadMixture` */
     expect(await BaseContract.connect(alice).isBase()).to.equal(true);
+  });
+  it('reverts - is owner?', async () => {
+    const { bob, BaseContract } = await loadFixture(deployContract);
+    await expect(BaseContract.connect(bob).isOwner()).to.be.reverted;
+    await expect(BaseContract.connect(bob).isOwner()).to.be.revertedWithCustomError(BaseContract, 'NotOwner');
+  });
+  it('success - is owner?', async () => {
+    const { BaseContract } = await loadFixture(deployContract);
+    await expect(BaseContract.isOwner()).to.emit(BaseContract, 'OwnerCall');
   });
 });
